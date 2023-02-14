4170fb885860b4a3c2c23a88061f1bcd64fdd4b5
Ano-via <93174923+Ano-via@users.noreply.github.com>
2023-02-14 13:39:10 +0800

    Update function.js

--- function.js
+++ function.js
@@ -56,24 +56,7 @@ function refresh() {
         }
     }
 }
-function getParameterByName(name) {
-    var url = window.location.href;
-    name = name.replace(/[\[\]]/g, "\\$&");
-    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
-  }
 
-var passwordinurl = getParameterById("password");
-var accountinurl = getParameterById("pastetext");
-if (passwordinurl) {
-	document.getElementById("password").innerHTML = passwordinurl;
-}
-if (accountinurl) {
-	document.getElementById("pastetext").innerHTML = accountinurl;
-}
 function normal8() {
     var chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     var passwordLength = 8;
