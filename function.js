6ad95d12eed9d9779872b7688373d7c8e29a34bb
Ano-via <93174923+Ano-via@users.noreply.github.com>
2023-06-08 17:53:11 +0800

    Update function.js

--- function.js
+++ function.js
@@ -147,7 +147,7 @@ function svttrack() {
             linksstr = linksstr.replace("\n", ",");
         }
     }
-	linksstr = linksstr.replace(/,,/g, ",");
+    linksstr = linksstr.replace(/,,/g, ",");
     linksstr = linksstr.replace(/Enter2Replace/g, "\n");
     if (linksstr.charAt(linksstr.length - 1) == ",") {
         linksstr = linksstr.substring(0, linksstr.length - 1);
@@ -173,7 +173,7 @@ function soetrack() {
             linksstr = linksstr.replace("\n", ",");
         }
     }
-	linksstr = linksstr.replace(/,,/g, ",");
+    linksstr = linksstr.replace(/,,/g, ",");
     linksstr = linksstr.replace(/Enter2Replace/g, "\n");
     if (linksstr.charAt(linksstr.length - 1) == ",") {
         linksstr = linksstr.substring(0, linksstr.length - 1);
@@ -189,61 +189,81 @@ function soetrack() {
 }
 
 function delparentheses() {
-	var linksstr = document.getElementById("linksstr").value;
-	var result = linksstr.replace(/ *\([^)]*\)/g, "");
-	document.getElementById("linksstr").value = result;
-	var copyText = document.getElementById("linksstr");
+    var linksstr = document.getElementById("linksstr").value;
+    var result = linksstr.replace(/ *\([^)]*\)/g, "");
+    document.getElementById("linksstr").value = result;
+    var copyText = document.getElementById("linksstr");
     copyText.select();
     copyText.setSelectionRange(0, 999);
     document.execCommand("copy");
-	document.getElementById("delparentheses").innerHTML = "√ 已复制";
-	var obj = document.getElementById('delparentheses');
-	obj.style.backgroundColor = "#daf2c2";
-	obj.style.color = "#397300";
-	setTimeout(function(){
-		obj.innerHTML = "去括号 + 复制";
-		obj.style.backgroundColor = "#f2f2f2";
-		obj.style.color = "#000000";
-	}, 3000);
+    document.getElementById("delparentheses").innerHTML = "√ 已复制";
+    var obj = document.getElementById('delparentheses');
+    obj.style.backgroundColor = "#daf2c2";
+    obj.style.color = "#397300";
+    setTimeout(function () {
+        obj.innerHTML = "去括号 + 复制";
+        obj.style.backgroundColor = "#f2f2f2";
+        obj.style.color = "#000000";
+    }, 3000);
+}
+
+function calculateUPCChecksum(digits) {
+    var sum = 0;
+    var parity = digits.length % 2;
+
+    for (var i = 0; i < digits.length; i++) {
+        var digit = parseInt(digits.charAt(i));
+        if (i % 2 === parity) {
+            sum += digit * 3;
+        } else {
+            sum += digit;
+        }
+    }
+
+    var checksum = (10 - (sum % 10)) % 10;
+    return checksum.toString();
 }
 
 function generateUPC() {
-	var linksstrElement = document.getElementById("linksstr");
-	var content = linksstrElement.value;
-	var numbers = content.replace(/[a-zA-Z-]/g, "");
-	var lines = numbers.split("\n");
-	var processedNumbers = [];
-	for (var i = 0; i < lines.length; i++) {
-		var number = lines[i];
-		if (number.length < 11) {
-			number = number.padEnd(11, "0");
-		}
-		else if (number.length > 11) {
-			number = number.substring(0, 11);
-		}
-		var sum = 0;
-		for (var j = 0; j < 11; j++) {
-			var digit = parseInt(number[j]);
-			sum += (j % 2 === 0) ? digit * 3 : digit;
-		}
-		var checkDigit = (10 - (sum % 10)) % 10;
-		number += checkDigit;
-		processedNumbers.push(number);
-	}
-	linksstrElement.value = processedNumbers.join("\n");
-	var copyText = document.getElementById("linksstr");
-		copyText.select();
-		copyText.setSelectionRange(0, 999);
-		document.execCommand("copy");
-	document.getElementById("bulkupc").innerHTML = "√ 已复制";
-	var obj = document.getElementById('bulkupc');
-	obj.style.backgroundColor = "#daf2c2";
-	obj.style.color = "#397300";
-	setTimeout(function(){
-		obj.innerHTML = '<img height="50%" src="https://img.icons8.com/pulsar-color/48/barcode.png" alt="upc"/>&nbsp;批量UPC';
-		obj.style.backgroundColor = "#f2f2f2";
-		obj.style.color = "#000000";
-	}, 3000);
+    // 获取id为"linksstr"的元素
+    var linksstrElement = document.getElementById("linksstr");
+
+    // 获取元素内容并提取数字
+    var content = linksstrElement.value;
+    var numbers = content.replace(/[a-zA-Z-]/g, "");
+    var lines = numbers.split("\n");
+    console.log(lines);
+    // 处理每个数字
+    var processedNumbers = [];
+    for (var i = 0; i < lines.length; i++) {
+        var number = lines[i];
+
+        // 如果数字不足11位，在末尾补0补足11位
+        if (number.length < 11) {
+            number = number.padEnd(11, "0");
+        }
+        // 如果数字超过11位，截取前11位数字
+        else if (number.length > 11) {
+            number = number.substring(0, 11);
+        }
+
+        // 计算UPC校验位
+        var sum = 0;
+        for (var j = 0; j < 11; j++) {
+            var digit = parseInt(number[j]);
+            sum += (j % 2 === 0) ? digit * 3 : digit;
+        }
+        var checkDigit = (10 - (sum % 10)) % 10;
+
+        // 添加UPC校验位到数字末尾
+        number += checkDigit;
+
+        processedNumbers.push(number);
+    }
+
+    // 更新id为"linksstr"的元素内容
+    linksstrElement.value = processedNumbers.join("\n");
+
 }
 
 function multiTranslate() {
@@ -308,4 +328,5 @@ function multiTranslate() {
     }
 
     translateText(0); // 开始翻译第一个语言
+}
 setInterval("refresh()", 1000);
